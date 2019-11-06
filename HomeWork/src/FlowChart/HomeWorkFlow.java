c005669f18741015615329152863d462c63542a8
Niv Ben Yair <niv12135@gmail.com>
2019-11-06 16:39:42 +0200

    Home Work Flow "If"

---
+++ HomeWork/src/FlowChart/HomeWorkFlow.java
@@ -0,0 +1,103 @@
+package FlowChart;
+
+public class HomeWorkFlow {
+
+	public static void main(String[] args) {
+
+//		exercise 9 page 15:
+//
+//		int random = (int) (Math.random() * 99) + 10;
+//
+//		int right = random % 10;
+//		int left = (int) random / 10;
+//
+//		System.out.printf("the number is: %d\nthe opposite is: %d%d", random, right, left);
+
+//		exercise 10 page 15: ******
+//
+//		double random = (Math.random() * 10) + 1;
+//
+//		if (random % 2 == 0) {
+//
+//			int randomDouble = (int) random + 2;
+//
+//			System.out.printf("the number is: %d\nthe new number is: %d", random, randomDouble);
+//		}
+//
+//		else {
+//
+//			int randomOdd = (int) random++;
+//
+//			System.out.printf("the number is: %d\nthe new number is: %d", random, randomOdd);
+//		}
+
+//		exercise 4 page 17:
+//
+//		int num1 = (int) (Math.random() * 10) + 0;
+//		int num2 = (int) (Math.random() * 10) + 0;
+//
+//		if (num1 % num2 == 0) {
+//			System.out.printf("the first num: %d\ndivide second num: %d", num1, num2);
+//		} else if (num2 % num1 == 0) {
+//			System.out.printf("the second num: %d\ndivide first num: %d", num2, num1);
+//		} else {
+//			System.out.println("no body divide");
+//		}
+
+//		exerice 5 page 17:
+
+//		int num1 = (int) (Math.random() * 10);
+//		int num2 = (int) (Math.random() * 10);
+//
+//		if (num1 > num2) {
+//			System.out.printf("the lowest num is: %d\nthe biggest num is: %d", num2, num1);
+//		} else {
+//			System.out.printf("the lowest num is: %d\nthe biggest num is: %d", num1, num2);
+//		}
+
+//		exerice 6 page 17:
+
+//		int salary = (int) (Math.random() * 6000) + 1000;
+//
+//		int newSalary = (int) (salary * 1.1);
+//
+//		if (newSalary > 6000) {
+//
+//			int salary2 = (int) (salary * 1.05);
+//			System.out.printf("the salary is: %d\nnew salary is: %d", salary, salary2);
+//		} else {
+//			System.out.printf("the salary is: %d\nnew salary is: %d", salary, newSalary);
+//		}
+//
+
+//		exerice 3 page 18:********
+
+//		int x = (int) (Math.random() * 10) + 0;
+//		int y = (int) (Math.random() * 10) + 0;
+//		int z = (int) (Math.random() * 10) + 0;
+//
+//		if (x > y) {
+//		} else if (x > z) {
+//			System.out.printf("its x: %d\nits z: %d\n its is y: %d", x, z, y);
+//		} else {
+//			System.out.printf("its z: %d\nits x: %d\n its y: %d", z, x, y);
+//			if (y > x) {
+//			} else if (y > z) {
+//				System.out.printf("its y: %d\nits z: %d\nits x: %d", y, z, x);
+//			} else {
+//				System.out.printf("its z: %d\nits y: %d\nits x: %d", z, y, x);
+//			}
+//		}
+//					
+//		exercise 4 page 20:
+
+//		int year = (int) (Math.random() * 2000) + 0;
+//		if ((year % 4 == 0) && (year % 100 != 0) || ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0))) {
+//			System.out.printf("yes this is LeapYear and the Year is: %d", year);
+//		} else {
+//			System.out.printf("not a LeapYear and the Year is: %d", year);
+//
+//		}
+
+	}
+}
